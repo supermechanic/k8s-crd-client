1ec64bac1da958b5ddf9a9347a5bc211e69fc6a7
machinista <yuanxiang.ma@outlook.com>
2021-01-12 11:52:37 +0800

    test case

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module model-service
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	k8s.io/apimachinery v0.20.0
+	k8s.io/client-go v0.20.0
+)
--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
 var kubeconfig *string
+var client dynamic.Interface
 
 func init() {
 	if home := homedir.HomeDir(); home != "" {
@@ -19,14 +27,55 @@ func init() {
 }
 
 //CreateSeldonDeploy //
-func CreateSeldonDeploy() {
-
+func CreateSeldonDeploy(name, modelname, server string) {
+	deploymentRes := schema.GroupVersionResource{Group: "machinelearning.seldon.io", Version: "v1", Resource: "deployments"}
+	deployment := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "machinelearning.seldon.io/v1",
+			"kind":       "SeldonDeployment",
+			"metadata": map[string]interface{}{
+				"name": name,
+			},
+			"spec": map[string]interface{}{
+				"annotations": map[string]interface{}{
+					"seldon.io/executor": "true",
+				},
+				"name": name + "-clf",
+				"predictors": map[string]interface{}{
+					"componentSpecs": nil,
+					"graph": map[string]interface{}{
+						"children":         nil,
+						"implementation":   server,
+						"modelUri":         "s3://" + modelname,
+						"envSecretRefName": "envSecretRefName",
+						"name":             "classifier",
+					},
+					"name":     "default",
+					"replicas": 1,
+				},
+			},
+		},
+	}
+	fmt.Println("Creating deployment...")
+	result, err := client.Resource(deploymentRes).Namespace("mayxe").Create(context.TODO(), deployment, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created deployment %q.\n", result.GetName())
 }
 func publishModel(c *gin.Context) {
-
+	CreateSeldonDeploy("iris-sk-v1", "iris", "SKLEARN_SERVER")
 }
 func main() {
 	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	client, err = dynamic.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
 	r := gin.Default()
 	r.POST("/api/v1/service/model/publish", publishModel)
 }
